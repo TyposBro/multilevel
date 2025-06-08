077a8bdef5e0fd4d9cd07d4d19be53e2265643e5
typosbro <typosbro@proton.me>
2025-06-09 02:15:49 +0900

    switch to full model for better pronounciation

--- android/app/src/main/java/com/typosbro/multilevel/features/inference/OnnxRuntimeManager.kt
+++ android/app/src/main/java/com/typosbro/multilevel/features/inference/OnnxRuntimeManager.kt
@@ -18,12 +18,12 @@ import java.io.OutputStream
 object OnnxRuntimeManager {
     private var environment: OrtEnvironment? = null
     private var session: OrtSession? = null
-//    private const val MODEL_FILE_NAME = "model.onnx"
+    private const val MODEL_FILE_NAME = "model.onnx"
 //    private const val MODEL_FILE_NAME = "model_f16.onnx"
 //    private const val MODEL_FILE_NAME = "model_q4.onnx"
 //    private const val MODEL_FILE_NAME = "model_q4f16.onnx"
 //    private const val MODEL_FILE_NAME = "model_q8f16.onnx"
-    private const val MODEL_FILE_NAME = "model_quantized.onnx"
+//    private const val MODEL_FILE_NAME = "model_quantized.onnx"
 //    private const val MODEL_FILE_NAME = "model_uint8.onnx"
 //    private const val MODEL_FILE_NAME = "model_uint8f16.onnx"
 
@@ -57,7 +57,7 @@ object OnnxRuntimeManager {
             }
 
             Log.d("OnnxRuntimeManager", "Copying model to cache: ${modelFile.absolutePath}")
-            inputStream = context.resources.openRawResource(R.raw.model_quantized) // Your model in res/raw
+            inputStream = context.resources.openRawResource(R.raw.model) // Your model in res/raw
             outputStream = FileOutputStream(modelFile)
             val buffer = ByteArray(4 * 1024) // 4K buffer
             var read: Int
